fbdaa25b31a2cc658fc71b58d7418e20eeab937c
Randall Degges <rdegges@home.lan>
2020-04-10 21:13:42 -0700

    Moving ubuntu packages to script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,24 @@
 printf "Installing pyenv..."
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
+# Install dependencies (Ubuntu)
+printf "Installing dependencies..."
+sudo apt install make \
+                 build-essential \
+                 libssl-dev \
+                 zlib1g-dev \
+                 libbz2-dev \
+                 libreadline-dev \
+                 libsqlite3-dev \
+                 wget \
+                 llvm \
+                 libncurses5-dev \
+                 libncursesw5-dev \
+                 xz-utils \
+                 tk-dev \
+                 libffi-dev \
+                 liblzma-dev
+
 # Install the latest core Python release
 printf "Installing latest stable release of Python..."
 release="$(pyenv install --list | sed 's/ //g' | grep '^[0-9]' | grep -v '[a-z]' | tail -n 1)"
